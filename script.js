16293294c84d175f6cb672f5a40fe9c3ad639baa
mrinmayee.rane <mrinmayee.rane@zeuslearning.com>
2021-02-14 19:03:50 +0530

    added quote functionality

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
 // Get Quote From API
-
 async function getQuote() {
-    const apiUrl = 'http://api.forismatic.com/api/1.0?format=json&method=getQuote&lang=en';
+    const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+    const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
     try {
-        const response = await fetch(apiUrl);
+        const response = await fetch(proxyUrl + apiUrl);
         const data = await response.json();
-        console.log(data);
+        authorText.innerText = data.quoteAuthor === '' ? 'Unknown' : data.quoteAuthor;
+        //Reduce font-size for long quotes
+        if (data.quoteText.length > 50){
+            quoteText.classList.add('long-quote');
+        } else {
+            quoteText.classList.remove('long-quote');
+        }
+        quoteText.innerText = data.quoteText;
     } catch(error){
+        getQuote();
         console.log('whoops, no quote', error);
     }
 }
