bb87c8b036c5862d417b24479e28425b3633c6b4
mrinmayee.rane <mrinmayee.rane@zeuslearning.com>
2021-02-17 00:36:27 +0530

    changed proxyUrl

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ function complete() {
 // Get Quote From API
 async function getQuote() {
     loading();
-    const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+    const proxyUrl = 'https://infinite-eyrie-93305.herokuapp.com/';
     const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
     try {
         const response = await fetch(proxyUrl + apiUrl);
